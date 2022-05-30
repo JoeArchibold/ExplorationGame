972c868add139b47f25bfb804a14ae96cbb679a0
JoeArchibold <mojojoey5555@gmail.com>
2022-05-30 11:34:39 -0600

    Changed map gen, added potential house gen

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById("canv");
 
-canvas.width = 800;
-canvas.height = 800;
+canvas.width = innerHeight;
+canvas.height = innerHeight;
 
 var c = canvas.getContext('2d');
 
@@ -13,8 +13,8 @@ let playerY = 12;
 var tileMap = [];
 
 function drawtileMap() {
-    c.clearRect(0,0,800,800);
-    let s = 800/25;
+    c.clearRect(0,0,innerHeight,innerHeight);
+    let s = innerHeight/25;
     for(let i = 0; i < 25; i++) {
         for(let j = 0; j < 25; j++) {
             if(i==playerX && j==playerY) {
@@ -44,6 +44,9 @@ function handleKeyPress(e) {
     if(e.key == 's' && playerY < 24) {
         playerY++;
     }
+    if(e.key == 'p') {
+        revealMap();
+    }
 
     updateVision();
 
@@ -56,17 +59,42 @@ function setup() {
     test = noise(100);
     console.log(test);
 
-    let noiseScale = 6;
+    let houseCount = 0;
+
+    //let noiseScale = 6;
     for(let i = 0; i < 25; i++) {
         tileMap[i] = [];
         for(let j = 0; j < 25; j++) {
-            let Noise = noise(i/noiseScale,j/noiseScale);
-            let ranCol = Math.min(300 * Noise, 255);
-            console.log(ranCol);
             tileMap[i][j] = {
-                color: "rgba(0,"+ ranCol + ",0,1)",
+                color: "",
                 explored: 1
             }
+
+            if(i > 10 && i < 14 && j > 10 && j < 14) {
+                tileMap[i][j].color = "rgba(0,200,0,1)"
+                continue;
+            }
+
+            if(Math.random() * 100 < 3 && houseCount < 10) {
+                tileMap[i][j].color = "rgba(100,0,200,1)"
+                houseCount++;
+                continue;
+            }
+
+            //let Noise = noise(i/noiseScale,j/noiseScale);
+            //let ranCol = Math.min(300 * Noise, 255);
+            let ranCol = Math.min(Math.random()*55+200, 255);
+            tileMap[i][j].color ="rgba(0,"+ ranCol + ",0,1)";
+
+            /* Noise Generation attempt
+            if(ranCol < 90) {
+                tileMap[i][j].color ="rgba(0,0,180,1)";                
+            } else if(ranCol < 110) {
+                tileMap[i][j].color ="#555";
+            } else {
+                tileMap[i][j].color ="rgba(0,"+ ranCol + ",0,1)";
+            } 
+            */
         }
     }
 
@@ -83,6 +111,14 @@ function updateVision() {
             let d = Math.sqrt(i**2+j**2);
             let cur = tileMap[playerX+i][playerY+j].explored;
             tileMap[playerX+i][playerY+j].explored = Math.min(cur ,((d-1.5)/(visD-1)));
+        }
+    }
+}
+
+function revealMap() {
+    for(let i = 0; i < 25; i++) {
+        for(let j = 0; j < 25; j++) {
+            tileMap[i][j].explored = 0;
         }
     }
 }
