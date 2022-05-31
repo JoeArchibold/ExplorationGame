fe9538651a44aee2f8db15e6f1bbdd5bc680df82
unknown <masonnieb@gmail.com>
2022-05-30 20:06:16 -0700

    ting :q testing testing

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ let playerX = 12;
 let playerY = 12;
 
 var tileMap = [];
+var tile = [];
 
 function drawtileMap() {
     c.clearRect(0,0,800,800);
@@ -44,6 +45,9 @@ function handleKeyPress(e) {
     if(e.key == 's' && playerY < 24) {
         playerY++;
     }
+    if(e.key == 'p') {
+        revealMap();
+    }
 
     updateVision();
 
@@ -55,21 +59,46 @@ function setup() {
     noCanvas();
     test = noise(100);
     console.log(test);
-
+    
     let noiseScale = 6;
+    let houseCount = 0;
     for(let i = 0; i < 25; i++) {
         tileMap[i] = [];
         for(let j = 0; j < 25; j++) {
-            let Noise = noise(i/noiseScale,j/noiseScale);
-            let ranCol = Math.min(300 * Noise, 255);
-            console.log(ranCol);
+
             tileMap[i][j] = {
-                color: "rgba(0,"+ ranCol + ",0,1)",
+                color: "rgba(255," + i*10 + ",255,1)",
                 explored: 1
+            }
+
+            // tileMap[i][j] = {
+            //     color: "",
+            //     explored: 1
+            // }
+
+            if(i > 10 && i < 14 && j > 10 && j < 14) {
+                tileMap[i][j].color = "rgba(0,200,0,1)"
+                continue;
+            }
+
+            if(Math.random() * 100 < 1.5 && houseCount < 10) {
+                tileMap[i][j].color = "rgba(100,0,200,1)"
+                houseCount++;
+                continue;
+            }
+
+            //let Noise = noise(i/noiseScale,j/noiseScale);
+            //let ranCol = Math.min(300 * Noise, 255);
+            let ranCol = Math.min(Math.random()*55+200, 255);
+            tileMap[i][j].color ="rgba(0,"+ ranCol + ",0,1)";
+            
+            console.log(ranCol);
+            if (ranCol >=180) {
+                tileMap[i][j].color = "rgba(0,"+ ranCol + ",0,1)";
             }
         }
     }
-
+    
     updateVision();
     drawtileMap();
 }
@@ -83,6 +112,14 @@ function updateVision() {
             let d = Math.sqrt(i**2+j**2);
             let cur = tileMap[playerX+i][playerY+j].explored;
             tileMap[playerX+i][playerY+j].explored = Math.min(cur ,((d-1.5)/(visD-1)));
+        }
+    }
+}
+
+function revealMap() {
+    for(let i = 0; i < 25; i++) {
+        for(let j = 0; j < 25; j++) {
+            tileMap[i][j].explored = 0;
         }
     }
 }
